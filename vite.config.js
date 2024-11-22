069f51c3b614a2fa8bfa28311a3df3240498444c
Thanhnam2005 <103724345+Thanhnam2005@users.noreply.github.com>
2024-11-22 13:27:47 +0700

    done

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Convert01/', // Replace with your repository name
+  base: '/Convert01/',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  }
 })
 
